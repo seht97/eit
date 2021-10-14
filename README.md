2773c24d96ef84405ebf396408179b2da6593751
Magnus Aagaard <magnus_aagaard@hotmail.com>
2021-10-14 13:46:42 +0200

    Updated landing detection to read pcd files. Updated README.md

--- README.md
+++ README.md
@@ -7,3 +7,14 @@
 4. Run `beit-ws`
 5. Launch simulation using `roslaunch eit posix.launch vehicle:=sdu_aed`
 6. To fix rviz being dumb run `rosrun tf static_transform_publisher 0 0 0 0 0 0 1 map camera_link 10` in a new terminal 
+
+## Install PCL library (system wide)
+```shell script
+sudo add-apt-repository ppa:sweptlaser/python3-pcl
+sudo apt update
+sudo apt install python3-pcl
+```
+To view pcd files use:
+```shell script
+pcl_viewer <filename>
+```
--- scripts/landing_detection_node.py
+++ scripts/landing_detection_node.py
@@ -1,10 +1,33 @@
-from pyntcloud import PyntCloud
+import pcl
 
 class LandingDetector:
     def __init__(self):
         # Initialize
-        pass
+        pc = pcl.load('/home/magnus/EiT/pointcloud_pcd_files/210.576000000.pcd')
 
+        #print(pc.size)
+
+        #fil = pc.make_passthrough_filter()
+        #fil.set_filter_field_name("z")
+        #fil.set_filter_limits(0, 2.5)
+        #cloud_filtered = fil.filter()
+        #print(cloud_filtered.size)
+        #pcl.save(cloud_filtered, '/home/magnus/EiT/filtered_pcd.pcd')
+        seg = pc.make_segmenter_normals(ksearch=50)
+        seg.set_optimize_coefficients(True)
+        seg.set_model_type(pcl.SACMODEL_NORMAL_PLANE)
+        seg.set_normal_distance_weight(0.1)
+        seg.set_method_type(pcl.SAC_RANSAC)
+        seg.set_max_iterations(100)
+        seg.set_distance_threshold(0.03)
+        indices, model = seg.segment()
+
+        print(model)
+
+        cloud_plane = pc.extract(indices, negative=False)
+        print(cloud_plane.size)
+        pcl.save(cloud_plane, '/home/magnus/EiT/segmented_pcd.pcd')
+        
     def detect(self):
         # Run detection
         pass
