68ac60928f6d4ffeef26744cfbddf56bea0ace6e
Magnus Aagaard <magnus_aagaard@hotmail.com>
2021-10-14 11:12:11 +0200

    Added landing detector class structure, no functionallity yet.

---
+++ scripts/landing_detection_node.py
@@ -0,0 +1,17 @@
+from pyntcloud import PyntCloud
+
+class LandingDetector:
+    def __init__(self):
+        # Initialize
+        pass
+
+    def detect(self):
+        # Run detection
+        pass
+
+def main():
+    ld = LandingDetector()
+    ld.detect()
+
+if __name__ == "__main__":
+    main()
