246f9189eb405678609faf72def9805101f7db90
Magnus Aagaard <magnus_aagaard@hotmail.com>
2021-10-28 11:37:08 +0200

    Changed test script and renamed it.

--- scripts/landing_detection_test_script.py
+++ scripts/landing_detection_test_script.py
@@ -9,6 +9,14 @@ class LandingDetector:
     def __init__(self):
         # Initialize
         self.pc = pcl.load('/home/magnus/EiT/pointcloud_pcd_files/210.576000000.pcd')
+
+    def add_noise(self):
+        pc = np.asarray(self.pc)
+        #print(pc)
+        pc[:,2] += 0.1*np.random.rand(pc.shape[0])
+        #print(pc)
+        self.pc = pcl.PointCloud(pc)
+        pcl.save(self.pc, '/home/magnus/EiT/noisy_pcd.pcd')
         
     def detect(self):
         # Run detection
@@ -21,6 +29,13 @@ class LandingDetector:
         fil.set_filter_limits(-DRONE_HEIGHT/2, DRONE_HEIGHT/2)
         cloud_filtered = fil.filter()
         pcl.save(cloud_filtered, '/home/magnus/EiT/filtered_pcd.pcd')
+        
+        # Smoothing
+        #fil = self.pc.make_statistical_outlier_filter()
+        #fil.set_mean_k(50)
+        #fil.set_std_dev_mul_thresh(1.0)
+        #cloud_filtered = fil.filter()
+        #pcl.save(cloud_filtered, '/home/magnus/EiT/filtered_pcd.pcd')
 
         seg = cloud_filtered.make_segmenter_normals(ksearch=50)
         seg.set_optimize_coefficients(True)
@@ -28,7 +43,7 @@ class LandingDetector:
         seg.set_normal_distance_weight(0.1)
         seg.set_method_type(pcl.SAC_RANSAC)
         seg.set_max_iterations(100)
-        seg.set_distance_threshold(0.03)
+        seg.set_distance_threshold(0.2)
         indices, model = seg.segment()
 
         cloud_plane = cloud_filtered.extract(indices, negative=False)
@@ -42,6 +57,7 @@ class LandingDetector:
 
 def main():
     ld = LandingDetector()
+    ld.add_noise()
     ld.detect()
 
 if __name__ == "__main__":
