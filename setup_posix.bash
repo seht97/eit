0181d83797a009fdd54487abefc4d2e77afce014
sh12 <seht97@gmail.com>
2021-11-10 12:52:00 +0100

    Changed setup script depending on pc

--- setup_posix.bash
+++ setup_posix.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Cleaning up.."
-if [ ! -z $1 ]
+
+if [ $1 = "M" ]
 then
 	: #Argument was given
 	echo "Running Magnus paths"
@@ -8,6 +9,13 @@ then
 	echo "Symlinking.."
 	ln -s /home/$USER/EiT/catkin_ws/src/eit/init.d-posix/* /home/$USER/EiT/PX4-Autopilot/ROMFS/px4fmu_common/init.d-posix/airframes/
 	ln -s /home/$USER/EiT/catkin_ws/src/eit/models/* /home/$USER/EiT/PX4-Autopilot/Tools/sitl_gazebo/models/
+elif [ $1 = "S" ]
+then
+	echo "Running Snørens paths"
+	find $PX4DIR -name "*_aed*" -delete
+	echo "Symlinking.."
+	ln -s $EITWSDIR/src/eit/init.d-posix/* $PX4DIR/ROMFS/px4fmu_common/init.d-posix/airframes/
+	ln -s $EITWSDIR/src/eit/models/* $PX4DIR/Tools/sitl_gazebo/models/
 else
 	: # Argument was not given
 	find ~/PX4-Autopilot/ -name "*_aed*" -delete
@@ -15,8 +23,3 @@ else
 	ln -s /home/$USER/eit_ws/src/eit/init.d-posix/* /home/$USER/PX4-Autopilot/ROMFS/px4fmu_common/init.d-posix/airframes/
 	ln -s /home/$USER/eit_ws/src/eit/models/* /home/$USER/PX4-Autopilot/Tools/sitl_gazebo/models/
 fi
-
-
-
-
-
